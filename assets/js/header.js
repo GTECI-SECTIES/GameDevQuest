a3f5b211012d2322f3e1b4b3b101316d99450c1a
Joyce Madalena <madalenajoycevs@gmail.com>
2025-05-07 15:58:02 -0300

    hotfix: correção menu sanduiche

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,3 +1,18 @@
+// <!--- Menu sanduiche --->
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const sideMenu = document.getElementById('side-menu');
+
+menuToggle.addEventListener('click', () => {
+  sideMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', (e) => {
+  if (!sideMenu.contains(e.target) && !menuToggle.contains(e.target)) {
+    sideMenu.classList.remove('active');
+  }
+});
 
 // <!--- Script Acessibilidade --->
 
